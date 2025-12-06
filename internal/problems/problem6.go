e4ad31da8294b11a5227e2387c020a27b380e2f1
MgShepherd <michaelshep1504@gmail.com>
2025-12-06 16:27:50 +0000

    Solved problem 6 part 2

--- internal/problems/problem6.go
+++ internal/problems/problem6.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+const MAX_COLS = 6
+
 func solveProblem6() (int, error) {
 	data, err := utils.ReadProblemFile(6)
 	if err != nil {
@@ -14,14 +16,11 @@ func solveProblem6() (int, error) {
 	}
 
 	lines := strings.Split(data, "\n")
-	for i := range lines {
-		lines[i] = strings.TrimSpace(lines[i])
-	}
 	var endIdx int
 	for endIdx = len(lines) - 1; len(lines[endIdx]) == 0; endIdx-- {
 	}
 
-	operations, err := getOperations(lines[endIdx])
+	operations, err := getOperations(strings.TrimSpace(lines[endIdx]))
 	if err != nil {
 		return -1, err
 	}
@@ -47,25 +46,106 @@ func getOperations(line string) ([]string, error) {
 
 func performArithmetic(operations, lines []string) ([]int, error) {
 	results := make([]int, len(operations))
+	problems := make([][]string, len(operations))
 
-	for _, line := range lines {
-		for i, el := range strings.Fields(line) {
-			intEl, err := strconv.Atoi(el)
-			if err != nil {
-				return []int{}, fmt.Errorf("[ERROR] Unable to convert %s to integer", el)
-			}
+	idxs := getSeperatorIndexes(lines)
 
-			if operations[i] == "+" {
-				results[i] += intEl
+	currentSepIdx := 0
+	var current strings.Builder
+	for i := range lines {
+		currentSepIdx = 0
+		for j := range lines[i] {
+			if currentSepIdx < len(idxs) && j == idxs[currentSepIdx] {
+				problems[currentSepIdx] = append(problems[currentSepIdx], current.String())
+				currentSepIdx++
+				current.Reset()
+			} else if lines[i][j] == ' ' {
+				current.WriteRune('0')
 			} else {
-				if results[i] == 0 {
-					results[i] = 1
-				}
-				results[i] *= intEl
+				current.WriteByte(lines[i][j])
 			}
+		}
+
+		if current.Len() > 0 {
+			problems[currentSepIdx] = append(problems[currentSepIdx], current.String())
+			current.Reset()
+		}
+	}
+
+	for i, problem := range problems {
+		operands, err := getOperands(problem)
+		if err != nil {
+			return []int{}, err
+		}
+
+		if operations[i] == "+" {
+			results[i] = getAdditionResult(operands)
+		} else {
+			results[i] = getMultiplicationResult(operands)
 		}
 	}
 	return results, nil
+}
+
+func getSeperatorIndexes(lines []string) []int {
+	var indexes []int
+	for i, el := range lines[0] {
+		if el == ' ' {
+			fullEmpty := true
+			for _, compare := range lines {
+				if compare[i] != ' ' {
+					fullEmpty = false
+					break
+				}
+			}
+			if fullEmpty {
+				indexes = append(indexes, i)
+			}
+		}
+	}
+	return indexes
+}
+
+func getAdditionResult(operands []int) int {
+	result := 0
+	for _, el := range operands {
+		result += el
+	}
+	return result
+}
+
+func getMultiplicationResult(operands []int) int {
+	result := 1
+	for _, el := range operands {
+		result *= el
+	}
+	return result
+}
+
+func getOperands(elements []string) ([]int, error) {
+	columnVals := make([]strings.Builder, MAX_COLS)
+	var operands []int
+
+	for _, element := range elements {
+		cols := strings.Split(element, "")
+		for i, el := range cols {
+			if el != "0" {
+				columnVals[len(cols)-1-i].WriteString(el)
+			}
+		}
+	}
+
+	for _, v := range columnVals {
+		if len(v.String()) == 0 {
+			break
+		}
+		intVal, err := strconv.Atoi(v.String())
+		if err != nil {
+			return []int{}, fmt.Errorf("[ERROR] Unable to convert %s to integer", v.String())
+		}
+		operands = append(operands, intVal)
+	}
+	return operands, nil
 }
 
 func sumResults(results []int) int {
